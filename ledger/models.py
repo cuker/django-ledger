4d3cdc49ca8f371ac8fa89e00cdf3a9e4c7e7a7d
Jason Kraus <jasonk@cukerinteractive.com>
2010-07-09 17:14:19 -0700

    simple is best

--- ledger/models.py
+++ ledger/models.py
@@ -1,65 +1,57 @@
 from django.db import models
-
-from dagcategory.models import DAGCategory
+from django.conf import settings
 
 from common import *
 
-PRECISION = {'max_digits':12, 'decimal_places':2} #TODO configurable precision
+PRECISION = getattr(settings, 'LEDGER_PRECISION', {'max_digits':12, 'decimal_places':2})
 
-class Account(DAGCategory):
+class AccountManager(models.Manager):
+    def create_transaction(self, credit_account, debit_account, amount):
+        if amount < 0:
+            raise LedgerException("Invalid amount")
+        if credit_account.polarity == debit_account.polarity:
+            raise LedgerException("Both accounts may not be of same type")
+        if credit_account.unit != debit_account.unit:
+            raise LedgerException("Both accounts must be of same unit")
+        credit_account._credit(amount)
+        debit_account._debit(amount)
+        transaction_type = credit_account.credit_transactions.model
+        return transaction_type(unit=self.unit,
+                                credit_account=credit_account,
+                                debit_account=debit_account,
+                                amount=amount,
+                                credit_balance=credit_acount.balance,
+                                debit_balance=debit_acount.balance,)
+
+class Account(models.Model):
     unit = models.CharField(max_length=5)
     balance = models.DecimalField(default=0, **PRECISION)
-    type = models.CharField(max_length=1, choices=ACCT_CHOICES)
+    polarity = models.CharField(max_length=1, choices=ACCT_CHOICES)
 
     modified = models.DateTimeField(auto_now=True, editable=False)
     created = models.DateTimeField(auto_now_add=True, editable=False)
+
+    objects = AccountManager()
 
     def _change_balance(self, amount):
         self.balance += amount
         if self.balance < 0:
             raise LedgerException("Transactions may not make the balance negative")
-        self.parents().filter(type=self.type).update(balance=models.F('balance')+amount)
         self.save()
-
-    def create_transaction(self, tx_type, other, amount):
-        if amount < 0:
-            raise LedgerException("Invalid amount")
-        if other.type == self.type:
-            raise LedgerException("Target acccount must be of different type")
-        if self.unit != other.unit:
-            raise LedgerException("Target acccount must be of same unit")
-        if tx_type not in dict(TX_CHOICES):
-            raise LedgerException("Unrecognized transaction type")
-        if self.type == ACCT_CREDIT:
-            credit_account = self
-            debit_account = other
-        elif self.type == ACCT_DEBIT:
-            credit_account = other
-            debit_account = self
-        if tx_type == TX_DEPOSIT:
-            credit_account._change_balance(amount)
-            debit_account._change_balance(amount)
-        elif tx_type == TX_WIDTHDRAW:
-            credit_account._change_balance(-amount)
-            debit_account._change_balance(-amount)
-        elif tx_type == TX_CREDIT:
-            credit_account._change_balance(amount)
-            debit_account._change_balance(-amount)
-        elif tx_type == TX_DEBIT:
-            credit_account._change_balance(-amount)
-            debit_account._change_balance(amount)
-        return self.transactions.create(unit=self.unit,
-                                        type=tx_type,
-                                        credit_account=credit_account,
-                                        debit_account=debit_account,
-                                        amount=amount,
-                                        credit_balance=credit_balance,
-                                        debit_balance=debit_balance,)
+    
+    def _credit(self, amount):
+        if self.polarity == ACCT_DEBIT:
+            amount *= -1
+        self._change_balance(amount)
+    
+    def _debit(self, amount):
+        if self.polarity == ACCT_CREDIT:
+            amount *= -1
+        self._change_balance(amount)
 
 class Transaction(models.Model):
     credit_account = models.ForeignKey(Account, related_name='credit_transactions')
     debit_account = models.ForeignKey(Account, related_name='debit_transactions')
-    type = models.CharField(max_length=1, choices=TX_CHOICES)
     
     unit = models.CharField(max_length=5)
     amount = models.DecimalField(**PRECISION)
